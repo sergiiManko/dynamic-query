4fa705ea824d3a64030fc3176645fd820b3c93f0
Sergii Manko <sergii.manko@gmail.com>
2019-01-14 16:52:45 +0200

    first commit

---
+++ src/main/java/com/mentor/jpa/dynamicquery/DynamicQueryApplication.java
@@ -0,0 +1,14 @@
+package com.mentor.jpa.dynamicquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicQueryApplication.class, args);
+	}
+
+}
+
