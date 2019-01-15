6789d31ca0178127566e72e110529ae27db78d5e
Sergii Manko <sergii.manko@drc-ukraine.org>
2019-01-15 15:44:52 +0200

    First part

--- src/main/java/com/mentor/jpa/dynamicquery/service/DepartmentService.java
+++ src/main/java/com/mentor/jpa/dynamicquery/service/DepartmentService.java
@@ -3,24 +3,33 @@ package com.mentor.jpa.dynamicquery.service;
 import com.mentor.jpa.dynamicquery.core.SearchData;
 import com.mentor.jpa.dynamicquery.domain.Department;
 import com.mentor.jpa.dynamicquery.repository.DepartmentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mentor.jpa.dynamicquery.specification.DepartmentSpecification;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Sergii Manko
+ */
 @Service
 public class DepartmentService {
-    @Autowired
-    private DepartmentRepository departmentRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public List<Department> getAllDepatmentBySearchData(final SearchData searchData) {
-        return departmentRepository.findAll((Specification<Department>) (root, criteriaQuery, criteriaBuilder) -> {
-            if (null == searchData.getFieldName() || null == searchData.getValue()) {
-                return criteriaBuilder.like("code");
-            } else {
-                return criteriaBuilder.equal(root.get(searchData.getFieldName()), searchData.getValue());
+    public DepartmentService(final DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getDynamicSearch(final List<SearchData> searchDataList) {
+        List<Department> result = new ArrayList<>();
+        Specification<Department> specification = new DepartmentSpecification();
+        if (searchDataList != null) {
+            for (SearchData searchData : searchDataList) {
+                specification = Specification.where(specification).and(new DepartmentSpecification(searchData));
             }
-        });
+            result = departmentRepository.findAll(specification);
+        }
+        return result;
     }
 }
