ec76b17baa80da5ded9c95ba4570a104ef6e9b76
akudria <ak98nneon@gmail.com>
2019-01-15 17:38:44 +0200

    Create department service, controller

--- src/main/java/com/mentor/jpa/dynamicquery/service/DepartmentService.java
+++ src/main/java/com/mentor/jpa/dynamicquery/service/DepartmentService.java
@@ -3,33 +3,24 @@ package com.mentor.jpa.dynamicquery.service;
 import com.mentor.jpa.dynamicquery.core.SearchData;
 import com.mentor.jpa.dynamicquery.domain.Department;
 import com.mentor.jpa.dynamicquery.repository.DepartmentRepository;
-import com.mentor.jpa.dynamicquery.specification.DepartmentSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Sergii Manko
- */
 @Service
 public class DepartmentService {
-    private final DepartmentRepository departmentRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
 
-    public DepartmentService(final DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
-    }
-
-    public List<Department> getDynamicSearch(final List<SearchData> searchDataList) {
-        List<Department> result = new ArrayList<>();
-        Specification<Department> specification = new DepartmentSpecification();
-        if (searchDataList != null) {
-            for (SearchData searchData : searchDataList) {
-                specification = Specification.where(specification).and(new DepartmentSpecification(searchData));
+    public List<Department> getAllDepatmentBySearchData(final SearchData searchData) {
+        return departmentRepository.findAll((Specification<Department>) (root, criteriaQuery, criteriaBuilder) -> {
+            if (null == searchData.getFieldName() || null == searchData.getValue()) {
+                return criteriaBuilder.like("code");
+            } else {
+                return criteriaBuilder.equal(root.get(searchData.getFieldName()), searchData.getValue());
             }
-            result = departmentRepository.findAll(specification);
-        }
-        return result;
+        });
     }
 }
