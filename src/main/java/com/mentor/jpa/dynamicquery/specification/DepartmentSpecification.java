fe284b9cbe62384963c891b61c461c7272e061af
Nikita Blyzniukov <blizniukov@gmail.com>
2019-01-16 13:40:41 +0200

    Add department Specification

--- src/main/java/com/mentor/jpa/dynamicquery/specification/DepartmentSpecification.java
+++ src/main/java/com/mentor/jpa/dynamicquery/specification/DepartmentSpecification.java
@@ -1,6 +1,8 @@
 package com.mentor.jpa.dynamicquery.specification;
 
+import com.mentor.jpa.dynamicquery.core.SearchData;
 import com.mentor.jpa.dynamicquery.domain.Department;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -9,15 +11,23 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 /**
- * @author Sergii Manko
+ * @author Nikita Blizniukov
  */
-public class DepartmentSpecification {
-    public static Specification<Department> departmantEqualsName(final String code) {
-        return new Specification<Department>() {
-            @Override
-            public Predicate toPredicate(Root<Department> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                return null;
-            }
-        };
+@NoArgsConstructor
+public class DepartmentSpecification implements Specification<Department> {
+    private transient SearchData searchData;
+
+    public DepartmentSpecification(final SearchData searchData) {
+        this.searchData = searchData;
+    }
+
+    @Override
+    public Predicate toPredicate(final Root<Department> root, final CriteriaQuery<?> criteriaQuery,
+                                 final CriteriaBuilder criteriaBuilder) {
+        if (searchData != null) {
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get(searchData.getFieldName())),
+                    "%" + searchData.getValue().toLowerCase() + "%");
+        }
+        return null;
     }
 }
