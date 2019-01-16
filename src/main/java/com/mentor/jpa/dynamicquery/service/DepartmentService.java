2016bfa4ea66eb4f5f8110fb06762fc879ab71d2
Nikita Blyzniukov <blizniukov@gmail.com>
2019-01-16 13:40:53 +0200

    Add department service

---
+++ src/main/java/com/mentor/jpa/dynamicquery/service/DepartmentService.java
@@ -0,0 +1,35 @@
+package com.mentor.jpa.dynamicquery.service;
+
+import com.mentor.jpa.dynamicquery.core.SearchData;
+import com.mentor.jpa.dynamicquery.domain.Department;
+import com.mentor.jpa.dynamicquery.repository.DepartmentRepository;
+import com.mentor.jpa.dynamicquery.specification.DepartmentSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Nikita Blizniukov
+ */
+@Service
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    @Autowired
+    public DepartmentService(final DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getDynamicSearch(List<SearchData> searchData) {
+        Specification<Department> specification = new DepartmentSpecification();
+
+        for (SearchData searchData1 : searchData) {
+            specification = Specification.where(specification).and(new DepartmentSpecification(searchData1));
+        }
+
+        return departmentRepository.findAll(specification);
+    }
+}
