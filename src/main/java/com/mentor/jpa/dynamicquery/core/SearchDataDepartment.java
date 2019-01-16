15c5f0036b1cd70b7c3173052141828d760762f1
Artem Kudria <ak98nneon@gmail.com>
2019-01-16 22:57:35 +0200

    Change all args constructor on my realization

--- src/main/java/com/mentor/jpa/dynamicquery/core/SearchDataDepartment.java
+++ src/main/java/com/mentor/jpa/dynamicquery/core/SearchDataDepartment.java
@@ -1,12 +1,10 @@
 package com.mentor.jpa.dynamicquery.core;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 /**
  * @author Artem Kudria
  */
-@AllArgsConstructor
 @NoArgsConstructor
 public final class SearchDataDepartment extends SearchData {
     public SearchDataDepartment(String fieldName, String value) {
--- src/main/java/com/mentor/jpa/dynamicquery/core/SearchDataEmployee.java
+++ src/main/java/com/mentor/jpa/dynamicquery/core/SearchDataEmployee.java
@@ -1,12 +1,10 @@
 package com.mentor.jpa.dynamicquery.core;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 /**
  * @author Artem Kudria
  */
-@AllArgsConstructor
 @NoArgsConstructor
 public final class SearchDataEmployee extends SearchData {
     public SearchDataEmployee(String fieldName, String value) {
