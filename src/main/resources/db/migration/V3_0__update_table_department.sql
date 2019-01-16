48e2ba220092518eefd2f970b17009443318373a
Nikita Blyzniukov <blizniukov@gmail.com>
2019-01-16 13:51:10 +0200

    Add script

---
+++ src/main/resources/db/migration/V3_0__update_table_department.sql
@@ -0,0 +1,10 @@
+ALTER TABLE department
+  ADD COLUMN unit VARCHAR(64);
+UPDATE department
+SET unit = 'default value'
+WHERE id = 1;
+UPDATE department
+SET unit = 'not default value'
+WHERE id = 2;
+ALTER TABLE department
+  ALTER COLUMN unit SET NOT NULL;
